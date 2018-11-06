49984d3cea38e5cfead3507c31ec04255006d477
Victor Duarte de Freitas <victor.freitas@oletecnologia.com.br>
2018-11-06 17:31:15 -0200

    Memento - início

---
+++ DesignPatterns.Domain/Models/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Domain.Models
+{
+    public class Carro : MementoModel<Carro>
+    {
+
+        public int AnoFabricacao { get; set; }
+        public int AnoModelo { get; set; }
+
+
+        public override void RestoreState(Carro original)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveState()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns.Domain/Models/MementoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Domain.Models
+{
+    public abstract class MementoModel<T>
+        where T : class
+    {
+        protected Stack<T> PreviousStates = new Stack<T>();
+        protected Stack<T> NextStates = new Stack<T>();
+
+        public abstract void SaveState();
+        public abstract void RestoreState(T original);
+
+        public bool CanUndo()
+        {
+            return PreviousStates.Count > 0;
+        }
+
+        public bool CanRedo()
+        {
+            return NextStates.Count > 0;
+        }
+    }
+}
